1b45438a8b18ce54358543bc5fd1164a3bc1aade
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-06-16 15:26:34 -0400

    add solution

--- OOPCardsSimpleViews/OOPCardsSimpleViews/CardSwitcherViewController.h
+++ OOPCardsSimpleViews/OOPCardsSimpleViews/CardSwitcherViewController.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "PlayingCard.h"
 
 @interface CardSwitcherViewController : UIViewController
 
+@property (strong, nonatomic) PlayingCard *threeOfSpades;
+@property (strong, nonatomic) PlayingCard *fourOfClubs;
+@property (strong, nonatomic) PlayingCard *eightOfDiamonds;
+@property (strong, nonatomic) PlayingCard *tenOfHearts;
+
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLabel;
+
 @end
+
--- OOPCardsSimpleViews/OOPCardsSimpleViews/CardSwitcherViewController.m
+++ OOPCardsSimpleViews/OOPCardsSimpleViews/CardSwitcherViewController.m
@@ -18,7 +18,38 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self setUp];
 }
 
+- (void)setUp
+{
+    self.threeOfSpades = [[PlayingCard alloc] initWithSuit:@"♠️" rank:@3];
+    self.fourOfClubs = [[PlayingCard alloc] initWithSuit:@"♣️" rank:@4];
+    self.eightOfDiamonds = [[PlayingCard alloc] initWithSuit:@"♦️" rank:@8];
+    self.tenOfHearts = [[PlayingCard alloc] initWithSuit:@"♥️" rank:@10];
+}
+
+-(void)changeCurrentPlayingCardTo:(PlayingCard *)playingCard
+{
+    self.topLabel.text = [NSString stringWithFormat:@"%@ %@", playingCard.rank, playingCard.suit];
+    self.middleLabel.text = [NSString stringWithFormat:@"%@ %@", playingCard.rank, playingCard.suit];
+    self.bottomLabel.text = [NSString stringWithFormat:@"%@ %@", playingCard.rank, playingCard.suit];
+}
+
+- (IBAction)threeOfSpadesTapped:(UIButton *)sender {
+    [self changeCurrentPlayingCardTo:self.threeOfSpades];
+}
+
+- (IBAction)fourOfClubsTapped:(id)sender {
+    [self changeCurrentPlayingCardTo:self.fourOfClubs];
+}
+
+- (IBAction)eightOfDiamondsTapped:(UIButton *)sender {
+    [self changeCurrentPlayingCardTo:self.eightOfDiamonds];
+}
+
+- (IBAction)tenOfHeartsTapped:(UIButton *)sender {
+    [self changeCurrentPlayingCardTo:self.tenOfHearts];
+}
 
 @end
