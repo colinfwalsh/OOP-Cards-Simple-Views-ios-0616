6a0c3d613c976a0597036c867e28d2efd12f4729
markedwardmurray <markedwardmurray@gmail.com>
2015-10-05 12:59:17 -0400

    replace solution

--- OOPCardsSimpleViews/FISAppDelegate.h
+++ OOPCardsSimpleViews/FISAppDelegate.h
@@ -1,4 +1,10 @@
+//
 //  FISAppDelegate.h
+//  OOPCardsSimpleViews
+//
+//  Created by Chris Gonzales on 6/16/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
 
 #import <UIKit/UIKit.h>
 
--- OOPCardsSimpleViews/FISCardSwitcherViewController.h
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.h
@@ -1,4 +1,10 @@
+//
 //  FISCardSwitcherViewController.h
+//  OOPCardsSimpleViews
+//
+//  Created by Chris Gonzales on 6/16/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
 
 #import <UIKit/UIKit.h>
 #import "FISCard.h"
@@ -9,6 +15,10 @@
 @property (strong, nonatomic) FISCard *fourOfClubs;
 @property (strong, nonatomic) FISCard *eightOfDiamonds;
 @property (strong, nonatomic) FISCard *tenOfHearts;
+
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLabel;
 
 @end
 
